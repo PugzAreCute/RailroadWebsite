90151ef290679118598d1625e8085d2a2c5aa459
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2021-02-13 10:45:06 -0800

    Make cookies betterer

--- js/Commons.js
+++ js/Commons.js
@@ -1,11 +1,13 @@
 //INIT Pref For Cookies
 const pref = getCookie("pref");
 
+let Cookie_Bool;
+
 //Execute function doStartupJS() which runs js on startup
 window.onload = doStartupJS();
 
 //Function Cookie
-var Cookie_Bool;
+
 function Cookie(){
   //Cookie policy
   let cookie = confirm("Do you agree to us using cookies to personalize user content. You can find the Cookie policy at https://putopug.github.io/RailroadWebsite/privacy/cookies.txt");
@@ -19,9 +21,8 @@ function doStartupJS(){
   switchVisible();
   if(Cookie_Bool){
     checkCookieTheme();
-  }
-  else {
-    document.cookie = "pref=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/RailroadWebsite;";
+  }else {
+    document.cookie = "pref=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/RailroadWebsite;";
   }
 }
 
@@ -32,7 +33,11 @@ function setCookie(cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   var expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/RailroadWebsite";
+  document.cookie = `${cname}=${cvalue};${expires};path=/RailroadWebsite`;
+}
+
+function unsetCookie(cname, cvalue){
+  document.cookie = `${cname}=${cvalue};expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/RailroadWebsite`;
 }
 
 //Function to get cookies
@@ -40,10 +45,7 @@ function getCookie(cname) {
   var name = cname + "=";
   var ca = document.cookie.split(';');
   for(var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
+    var c = ca[i].trim();
     if (c.indexOf(name) == 0) {
       return c.substring(name.length, c.length);
     }
@@ -76,15 +78,20 @@ function switchVisible() {
       document.getElementById('light_mode').style.display = 'block';
       document.getElementById('dark_mode').style.display = 'none';
       if (Cookie_Bool) {
-        setCookie("pref", "light", 365)
+        //unsetCookie("pref", )
+        setCookie("pref", "light", 365);
+        
+        //unset dark cookie
       } else {
-        document.getElementById('light_mode').style.display = 'none';
-        document.getElementById('dark_mode').style.display = 'block';
-        if (Cookie_Bool) {
-          setCookie("pref", "dark", 365)
-        }
       }
-
+    }else if (document.getElementById('dark_mode').style.display == 'none') {
+      document.getElementById('dark_mode').style.display = 'block';
+      document.getElementById('light_mode').style.display = 'none';
+      if (Cookie_Bool) {
+        setCookie("pref", "dark", 365);
+        //unset light cookie
+      } else {
+      }
     }
     const element = document.body;
     element.classList.toggle("dark_mode");
