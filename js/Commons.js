6de65b99bf641d6dfe80e3f383fc0024804a9caa
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-02-14 14:51:34 +0530

    Made Navbar Common (NavBar.html)

--- js/Commons.js
+++ js/Commons.js
@@ -17,6 +17,7 @@ function Cookie(){
 
 //Declare function doStartupJS()
 function doStartupJS(){
+  includeHTML();
   if (Cookie_Bool == undefined)
     Cookie();
   switchVisible();
@@ -93,3 +94,31 @@ function setMode(){
   if (!Cookie_Bool || getCookie("pref") == "dark" != document.body.classList.contains('dark_mode'))
     document.body.classList.toggle("dark_mode");
 }
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
