1f5d4cdb56d54e68554e4e0fe432a95fea3c5e69
PugzAreCute <me@pugzarecute.com>
2021-10-15 07:16:54 +0530

    More mess

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function secureServer(id) {
-  var element = document.getElementById(id);
-  if (element.getAttribute("class") === "secureServer") {
-    element.setAttribute("class", "")
-    return;
-  }
-  element.setAttribute("class", "secureServer")
-
-}
